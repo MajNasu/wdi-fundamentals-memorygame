d48e584d198e6e8daa22eb83c78dcbace849ad1a
Susan <cho@Choui-MacBook-Pro-55.local>
2017-06-22 23:44:25 -0400

    created game logic 2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var inPlay = [];
 
-var c1 = "king";
-var c2 = "king";
-var c3 = "queen";
-var c4 = "queen";
-
+var c1 = cards[0];
+inPlay.push(c1);
 console.log("User flipped " + c1);
+
+var c2 = cards[2];
+inPlay.push(c2);
+console.log("User flipped " + c2);
+
+if(inPlay.length === 2){
+	if(inPlay[0] === inPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
