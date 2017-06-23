3410f2161d48a4aac02cc2abc177b6d730d9a7ed
Susan <cho@Choui-MacBook-Pro-55.local>
2017-06-23 00:04:36 -0400

    created game logic 3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var inPlay = [];
 
-var c1 = cards[0];
-inPlay.push(c1);
-console.log("User flipped " + c1);
-
-var c2 = cards[2];
-inPlay.push(c2);
-console.log("User flipped " + c2);
-
-if(inPlay.length === 2){
+function checkForMatch(){
 	if(inPlay[0] === inPlay[1]){
-		alert("You found a match!");
+		alert("You have found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+function flipCard(cardID){
+	console.log("User flipped " + cards[cardID]);
+	inPlay.push(cards[cardID]);
+
+checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);
